ddfadb96362e8d0a7c0cf268029c6fb4877c88a7
pshreya747 <pshreya0711@gmail.com>
2024-09-29 01:16:56 +0530

    Implementation of some basic stuff

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "@/assests/styles/globals.css";
+
+export const metadata = {
+  title: "Property Pulse | Find The Perfect Rental",
+  description: "Find your dream rental property",
+  keywords: "rental, find rentals, find properties",
+};
+
+const layout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+};
+
+export default layout;
