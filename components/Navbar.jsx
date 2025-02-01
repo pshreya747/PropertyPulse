744beb272e577f762bcc951a9cfb101f0325b36c
pshreya747 <pshreya0711@gmail.com>
2025-02-01 20:17:46 +0530

    Added dropdowns and links.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -19,14 +19,14 @@ const Navbar = () => {
 
   const pathname = usePathname();
 
-  useEffect(() => {
-    const setAuthProviders = async () => {
-      const res = await getProviders();
-      setProviders(res);
-    };
+  // useEffect(() => {
+  //   const setAuthProviders = async () => {
+  //     const res = await getProviders();
+  //     setProviders(res);
+  //   };
 
-    setAuthProviders();
-  }, []);
+  //   setAuthProviders();
+  // }, []);
 
   return (
     <nav className="bg-blue-700 border-b border-blue-500">
@@ -69,6 +69,7 @@ const Navbar = () => {
                 PropertyPulse
               </span>
             </Link>
+
             {/* <!-- Desktop Menu Hidden below md screens --> */}
             <div className="hidden md:ml-6 md:block">
               <div className="flex space-x-2">
@@ -88,7 +89,7 @@ const Navbar = () => {
                 >
                   Properties
                 </Link>
-                {/* {session  */}
+                {/* session  */}
                 && (
                 <Link
                   href="/properties/add"
@@ -103,126 +104,131 @@ const Navbar = () => {
             </div>
           </div>
           {/* <!-- Right Side Menu (Logged Out) --> */}
-          {/* {!session  */}
-          && (
-          <div className="hidden md:block md:ml-6">
-            <div className="flex items-center">
-              {providers &&
-                Object.values(providers).map((provider, index) => (
-                  <button
-                    onClick={() => signIn(provider.id)}
-                    key={index}
-                    className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >
-                    <FaGoogle className="text-white mr-2" />
-                    <span>Login or Register</span>
-                  </button>
-                ))}
+          {
+            // !session &&
+            <div className="hidden md:block md:ml-6">
+              <div className="flex items-center">
+                {providers &&
+                  Object.values(providers).map((provider, index) => (
+                    <button
+                      onClick={() => signIn(provider.id)}
+                      key={index}
+                      className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                    >
+                      <FaGoogle className="text-white mr-2" />
+                      <span>Login or Register</span>
+                    </button>
+                  ))}
+              </div>
             </div>
-          </div>
-          ){/* } */}
+          }
           {/* <!-- Right Side Menu (Logged In) --> */}
-          {/* {session  */}
-          && (
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
-            <Link href="/messages" className="relative group">
-              <button
-                type="button"
-                className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-              >
-                <span className="absolute -inset-1.5"></span>
-                <span className="sr-only">View notifications</span>
-                <svg
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-                  />
-                </svg>
-              </button>
-              {/* <UnreadMessageCount session={session} /> */}
-            </Link>
-            {/* <!-- Profile dropdown button --> */}
-            <div className="relative ml-3">
-              <div>
+          {
+            // { session } &&
+            <div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
+              <Link href="/messages" className="relative group">
                 <button
                   type="button"
-                  className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                  id="user-menu-button"
-                  aria-expanded={isProfileMenuOpen}
-                  aria-haspopup="true"
-                  onClick={() => setIsProfileMenuOpen((prev) => !prev)}
+                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                 >
                   <span className="absolute -inset-1.5"></span>
-                  <span className="sr-only">Open user menu</span>
-                  <Image
-                    className="h-8 w-8 rounded-full"
-                    // src={profileImage || profileDefault}
-                    alt=""
-                    width={40}
-                    height={40}
-                  />
+                  <span className="sr-only">View notifications</span>
+                  <svg
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+                    />
+                  </svg>
                 </button>
-              </div>
+                <UnreadMessageCount />
+                {/* <UnreadMessageCount session={session} /> */}
+              </Link>
 
-              {/* <!-- Profile dropdown --> */}
-              {isProfileMenuOpen && (
-                <div
-                  id="user-menu"
-                  className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                  role="menu"
-                  aria-orientation="vertical"
-                  aria-labelledby="user-menu-button"
-                  tabIndex="-1"
-                >
-                  <Link
-                    href="/profile"
-                    className="block px-4 py-2 text-sm text-gray-700"
-                    role="menuitem"
-                    tabIndex="-1"
-                    id="user-menu-item-0"
-                    onClick={() => {
-                      setIsProfileMenuOpen(false);
-                    }}
-                  >
-                    Your Profile
-                  </Link>
-                  <Link
-                    href="/properties/saved"
-                    className="block px-4 py-2 text-sm text-gray-700"
-                    role="menuitem"
-                    tabIndex="-1"
-                    id="user-menu-item-2"
-                    onClick={() => {
-                      setIsProfileMenuOpen(false);
-                    }}
-                  >
-                    Saved Properties
-                  </Link>
+              {/* <!-- Profile dropdown button --> */}
+              <div className="relative ml-3">
+                <div>
                   <button
-                    onClick={() => {
-                      setIsProfileMenuOpen(false);
-                      signOut();
-                    }}
-                    className="block px-4 py-2 text-sm text-gray-700"
-                    role="menuitem"
-                    tabIndex="-1"
-                    id="user-menu-item-2"
+                    type="button"
+                    className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                    id="user-menu-button"
+                    aria-expanded={isProfileMenuOpen}
+                    aria-haspopup="true"
+                    onClick={() => setIsProfileMenuOpen((prev) => !prev)}
                   >
-                    Sign Out
+                    <span className="absolute -inset-1.5"></span>
+                    <span className="sr-only">Open user menu</span>
+                    <Image
+                      className="h-8 w-8 rounded-full"
+                      src={
+                        // profileImage ||
+                        profileDefault
+                      }
+                      alt=""
+                      width={40}
+                      height={40}
+                    />
                   </button>
                 </div>
-              )}
+
+                {/* <!-- Profile dropdown --> */}
+                {isProfileMenuOpen && (
+                  <div
+                    id="user-menu"
+                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                    role="menu"
+                    aria-orientation="vertical"
+                    aria-labelledby="user-menu-button"
+                    tabIndex="-1"
+                  >
+                    <Link
+                      href="/profile"
+                      className="block px-4 py-2 text-sm text-gray-700"
+                      role="menuitem"
+                      tabIndex="-1"
+                      id="user-menu-item-0"
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                      }}
+                    >
+                      Your Profile
+                    </Link>
+                    <Link
+                      href="/properties/saved"
+                      className="block px-4 py-2 text-sm text-gray-700"
+                      role="menuitem"
+                      tabIndex="-1"
+                      id="user-menu-item-2"
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                      }}
+                    >
+                      Saved Properties
+                    </Link>
+                    <button
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                        signOut();
+                      }}
+                      className="block px-4 py-2 text-sm text-gray-700"
+                      role="menuitem"
+                      tabIndex="-1"
+                      id="user-menu-item-2"
+                    >
+                      Sign Out
+                    </button>
+                  </div>
+                )}
+              </div>
             </div>
-          </div>
-          ){/* } */}
+          }
         </div>
       </div>
 
@@ -246,7 +252,8 @@ const Navbar = () => {
             >
               Properties
             </Link>
-            {session && (
+            {
+              // session &&
               <Link
                 href="/properties/add"
                 className={`${
@@ -255,19 +262,21 @@ const Navbar = () => {
               >
                 Add Property
               </Link>
-            )}
+            }
 
-            {/* {!session &&
+            {/* {
+            !session &&
               providers &&
-              Object.values(providers).map((provider, index) => (
-                <button
-                  onClick={() => signIn(provider.id)}
-                  key={index}
-                  className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                >
-                  <span>Login or Register</span>
-                </button>
-              ))} */}
+              Object.values(providers).map((provider, index) =>
+                 ( */}
+            <button
+              // onClick={() => signIn(provider.id)}
+              // key={index}
+              className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+            >
+              <span>Login or Register</span>
+            </button>
+            {/* ))} */}
           </div>
         </div>
       )}
