3d9a62cd4853f78b52556205448442dba710ced7
pshreya747 <pshreya0711@gmail.com>
2025-02-03 00:08:38 +0530

    Homepage Componets.

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,17 @@
 import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
 import Link from "next/link";
 
 const HomePage = () => {
   return (
-    // <>
-    //   <Hero />
-    // </>
-    <div>
-      <h1 className="text-3xl">Welcome</h1>
-      <Link href="/properties">Show Properties</Link>
-    </div>
+    <>
+      <Hero />
+      <InfoBoxes />
+    </>
+    // <div>
+    //   <h1 className="text-3xl">Welcome</h1>
+    //   <Link href="/properties">Show Properties</Link>
+    // </div>
   );
 };
 
