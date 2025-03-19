be778c4b1292c0a5d10d57693b513261d0b9e581
pshreya747 <pshreya0711@gmail.com>
2025-03-19 16:03:44 +0530

    Implemented Share buttons and search functionality.

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,26 +1,36 @@
 import PropertyCard from "@/components/PropertyCard";
 // import properties from "@/properties.json";
 import { fetchProperties } from "@/utils/requests";
+import PropertySearchForm from "@/components/PropertySearchForm";
 
 const PropertiesPage = async () => {
   const properties = await fetchProperties();
 
   //sort properties by date
   properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
   return (
-    <section className="px-4 py-6">
-      <div className="container-xl lg:container m-auto px-4 py-6">
-        {properties.length === 0 ? (
-          <p>NO properties found.</p>
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {properties.map((property) => (
-              <PropertyCard key={property._id} property={property} />
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+    <>
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+          <PropertySearchForm />
+        </div>
+      </section>
+
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          {properties.length === 0 ? (
+            <p>NO properties found.</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
   );
 };
 
