faf8d2d77c2661cd32884ad658f39b681a069802
pshreya747 <pshreya0711@gmail.com>
2025-03-20 16:25:32 +0530

    Added featured properties component.

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -4,7 +4,7 @@ import PropertyCard from "../components/PropertyCard";
 import { fetchProperties } from "@/utils/requests";
 
 const HomeProperties = async () => {
-  const properties = await fetchProperties();
+  const data = await fetchProperties();
   const recentProperties = data.properties
     .sort(() => Math.random() - Math.random())
     .slice(0, 3);
